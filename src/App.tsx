1e6c26e33d03a102168c44fb7672eedcc5e8720c
Antti Romppanen <romppanen.antti@gmail.com>
2023-09-13 16:29:03 +0300

    fix img test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TextSection from "./components/TextSection";
 
 function App() {
   return (
-    <div className="h-full w-full bg-userRed bg-[url(bg-intro-mobile.png)] bg-contain md:h-screen md:bg-[url(bg-intro-desktop.png)]">
+    <div className="h-full w-full bg-userRed bg-[url(/bg-intro-mobile.png)] bg-contain md:h-screen md:bg-[url(/bg-intro-desktop.png)]">
       <div className="m-auto max-w-[1200px] px-6 md:px-16">
         <div className="flex flex-col items-center justify-center gap-y-16 pt-24 text-center md:mt-0 md:flex-row md:gap-x-4 md:text-left">
           <TextSection />
